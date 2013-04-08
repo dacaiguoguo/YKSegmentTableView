a6cf6310ada59a064a273e5d037d026d7d14e4ab
dacaiguoguo <dacaiguoguo@163.com>
2013-04-08 15:52:03 +0800

    Initial Commit

---
+++ YKSegmentTableViewTests/YKSegmentTableViewTests.h
@@ -0,0 +1,13 @@
+//
+//  YKSegmentTableViewTests.h
+//  YKSegmentTableViewTests
+//
+//  Created by yanguo.sun on 13-4-8.
+//  Copyright (c) 2013年 YEK. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YKSegmentTableViewTests : SenTestCase
+
+@end
---
+++ YKSegmentTableViewTests/YKSegmentTableViewTests.m
@@ -0,0 +1,32 @@
+//
+//  YKSegmentTableViewTests.m
+//  YKSegmentTableViewTests
+//
+//  Created by yanguo.sun on 13-4-8.
+//  Copyright (c) 2013年 YEK. All rights reserved.
+//
+
+#import "YKSegmentTableViewTests.h"
+
+@implementation YKSegmentTableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in YKSegmentTableViewTests");
+}
+
+@end
